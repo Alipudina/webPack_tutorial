b14cb5365d98bd00d73f64341140ef6c01d5a1fe
Alipudina <alipudina333@gmail.com>
2019-01-14 09:55:09 +0100

    first commit after runninig

---
+++ src/scripts/factory.js
@@ -0,0 +1,8 @@
+export const addElement = (parent, elem, text) => {
+  const newElement = document.createElement(elem);
+  newElement.innerText = text;
+  parent.appendChild(newElement);
+}
+
+// addElement(list, LI, 'A new task was added');
+export const addition = (a, b) => a+b;
---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/scripts/app.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    // set up tasks here
+    rules: [
+      {
+        test: /\.js?/i,
+        exclude: /node_modules/i,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+  plugins: []
+}
